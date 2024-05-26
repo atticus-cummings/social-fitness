ba7bdbc4f24695d882b8d99bc7cdda79a89b915c
Atticus Cummings <atticuscummings@ucla.edu>
2024-05-26 15:40:14 -0700

    Added caption to post, fit post to image size

--- src/components/feed.js
+++ src/components/feed.js
@@ -10,52 +10,82 @@ export default function Feed({ supabase, session }) {
     console.log("USER ID:", userId)
 
     //on certain events (not all), the homepage feed will update in real time 
-    const {data, mutate} = useSWR(`image-${userId}`,
-        async() => await fetchData()
+    const { data, mutate } = useSWR(`image-${userId}`,
+        async () => await fetchData()
     );
     //gets all the data needed for a relevant feed
     async function fetchData() {
         //gets all the uuids of all of a users following
-        const {data: followingUserIds} = await supabase
+        const { data: followingUserIdsData, error: followingError } = await supabase
             .from('followers')
             .select('following_user_id')
             .eq('user_id', userId)
-//           .sort() TODO
-            .throwOnError();
+        //           .sort() TODO
+        if (followingError) throw followingError;
 
-        followingUserIds.push(userId)
-    
+
+        const followingUserIds = followingUserIdsData.map(user => user.following_user_id);
+        followingUserIds.push(userId);
+
         console.log("followingUserIds:", followingUserIds);
-        const {data:fileIdArray} = await supabase
+       
+        //get file IDS
+        const { data: fileIdArray, error: fileIdError} = await supabase
             .from('file_upload_metadata')
             .select('id')
             .in('user_id', followingUserIds)
-            .throwOnError();
-        const ids = fileIdArray.map(file => file.id);
-        const { data, error } = await supabase
+    
+        if(fileIdError) throw fileIdError;
+         const ids = fileIdArray.map(file => file.id);
+
+// get captions
+        const { data: captionArray, error: captionError } = await supabase
+            .from('file_upload_metadata')
+            .select('id, caption_text')
+            .in('id', ids)
+
+        if (captionError) throw captionError;
+
+        console.log("captions array: ", captionArray)
+
+        const captionsMap = captionArray.reduce((map, item) => {
+            map[item.id] = item.caption_text;
+            return map;
+        }, {});
+
+        //get urls
+        const { data, error: urlError } = await supabase
             .storage
             .from('media')
             .createSignedUrls(ids, 60); // Adjust file name and expiry time as needed
 
-            
+            if(urlError) throw urlError;
+
+        console.log("Captions Map:", captionsMap);
+
         if (data) {
-            const signedURLs = data.map(item => item.signedUrl);
-            return signedURLs;
-        } 
+            const combinedData = data.map((file,index) => ({
+                id: file.id,
+                signedUrl: file.signedUrl,
+                caption: captionsMap[[ids[index]]] || '',
+            }));
+            console.log("Combined Data:",combinedData);
+
+            return combinedData;
+        }
         else {
             return null;
-            return error;
         }
-    } 
+    }
     console.log("DATA HERE", data)
     return (
-        <div>
-            {data === null ? <>You have no data to show!</> :data?.map((item, index) => (
-                    <div className='post'>
-                    <img key={index}  src={item} style={{ width: '468px' }}/>
-                    <p></p>
-                    </div>
-                ))}
+        <div className='socialFeed'>
+            {data === null ? <>You have no data to show!</> : data?.map((item, index) => (
+                <div className='post' key={index}>
+                    <img src={item.signedUrl} style={{ width: '468px' }} />
+                    <div className="caption">{item.caption}</div>
+                </div>
+            ))}
         </div>
     );
 }
