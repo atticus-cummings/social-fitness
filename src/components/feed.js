78840646997fac826b68146bc396658b31727191
Atticus Cummings <atticuscummings@ucla.edu>
2024-05-28 14:24:24 -0700

    Code cleanup, added comments, adjusted comment css

--- src/components/feed.js
+++ src/components/feed.js
@@ -9,7 +9,6 @@ import { FaDumbbell } from "react-icons/fa";
 export default function Feed({ supabase, session }) {
     const [comment, setComment] = useState('');
 
-
     //get the current user id
     const userId = session.user.id
     console.log("USER ID:", userId)
@@ -20,25 +19,29 @@ export default function Feed({ supabase, session }) {
     );
     //gets all the data needed for a relevant feed
 
-    const handleLike = async (likeCount, fileId) => { 
-       likeCount = likeCount + 1;
+    //Increment like 
+    const handleLike = async (likeCount, fileId) => {
+        likeCount = likeCount + 1;
         await supabase
             .from('file_upload_metadata')
             .update({ like_count: likeCount })
             .eq('id', fileId);
-        //modify so it can only be liked once. 
+        //TODO: modify so it can only be liked once. 
+        //TODO: Make it so new like-count displays (semi-optional)
         console.log("Liked!");
     }
-
+    //Set comment value
     const handleCommentInput = (event) => {
         setComment(event.target.value);
     };
+
+    //submit Comment to data base
     const handleCommentSubmit = async (fileId) => {
         if (comment) {
             await supabase
-            .from('comments')
-            .insert({file_id: fileId, author_id: userId, comment_text: comment })
-            .throwOnError();
+                .from('comments')
+                .insert({ file_id: fileId, author_id: userId, comment_text: comment })
+                .throwOnError();
         }
         setComment(null);
     };
@@ -51,41 +54,41 @@ export default function Feed({ supabase, session }) {
                 .from('followers')
                 .select('following_user_id')
                 .eq('user_id', userId);
-    
+
             if (followingError) throw followingError;
-    
+
             const followingUserIds = followingUserIdsData.map(user => user.following_user_id);
             followingUserIds.push(userId); // Include the current user
-           // console.log("followingUserIds:", followingUserIds);
-    
+            // console.log("followingUserIds:", followingUserIds);
+
             // Fetch file metadata
             const { data: fileMetadata, error: fileIdError } = await supabase
                 .from('file_upload_metadata')
                 .select('id, user_id, caption_text, like_count, created_at')
                 .in('user_id', followingUserIds);
-    
+
             if (fileIdError) throw fileIdError;
-         //   console.log("Metadata:", fileMetadata);
-    
+                //   console.log("Metadata:", fileMetadata);
+
             // Sort metadata by creation date (latest first)
             const sortedMetadata = fileMetadata.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-          //  console.log("Sorted Metadata:", sortedMetadata);
-    
+                //  console.log("Sorted Metadata:", sortedMetadata);
+
             const ids = sortedMetadata.map(item => item.id);
-           // console.log("Ids:", ids);
-    
+                // console.log("Ids:", ids);
+
             const fileIdUserIdMap = sortedMetadata.reduce((map, item) => {
                 map[item.id] = item.user_id;
                 return map;
             }, {});
-          //  console.log("fileIdtoUserId", fileIdUserIdMap);
-    
+                //  console.log("fileIdtoUserId", fileIdUserIdMap);
+
             const captionsMap = sortedMetadata.reduce((map, item) => {
                 map[item.id] = item.caption_text;
                 return map;
             }, {});
-          //  console.log("Captions Map:", captionsMap);
-    
+            //  console.log("Captions Map:", captionsMap);
+
             // Fetch usernames
             const [{ data: usernameArray, error: usernameError }] = await Promise.all([
                 supabase
@@ -93,32 +96,32 @@ export default function Feed({ supabase, session }) {
                     .select('id, username')
                     .in('id', followingUserIds)
             ]);
-    
+
             if (usernameError) throw usernameError;
-    
+
             const usernamesMap = usernameArray.reduce((map, item) => {
                 map[item.id] = item.username;
                 return map;
             }, {});
-          //  console.log("Usernames:", usernamesMap);
-    
+            //  console.log("Usernames:", usernamesMap);
+
             // Fetch signed URLs
             const { data: urlData, error: urlError } = await supabase
                 .storage
                 .from('media')
-                .createSignedUrls(ids, 60); 
-    
+                .createSignedUrls(ids, 60);
+
             if (urlError) throw urlError;
 
-    
+            //format url data into map 
             if (urlData) {
                 console.log("urlData", urlData);
-                const urlMap = urlData.reduce((map,item)=>{
+                const urlMap = urlData.reduce((map, item) => {
                     map[item.path] = item.signedUrl;
                     return map;
                 })
-              //  console.log("urlMap", urlMap);
-                
+                //  console.log("urlMap", urlMap);
+
                 const combinedData = sortedMetadata.map((item, index) => ({
                     id: item.id,
                     signedUrl: urlMap[item.id],
@@ -126,8 +129,8 @@ export default function Feed({ supabase, session }) {
                     caption: item.caption_text,
                     likes: item.like_count,
                 }));
-               // console.log("Combined Data:", combinedData);
-    
+                // console.log("Combined Data:", combinedData);
+
                 return combinedData;
             } else {
                 return null;
@@ -137,30 +140,30 @@ export default function Feed({ supabase, session }) {
             return null;
         }
     }
-    
-   // console.log("DATA HERE", data)
+
+    // console.log("DATA HERE", data)
     return (
         <div className='socialFeed'>
             {data === null ? <>You have no data to show!</> : data?.map((item, index) => (
                 <div className='post' key={index}>
                     <div className="username">User: {item.username}</div>
-                    <img src={item.signedUrl} style={{ width: '600px' } } className='center' />
+                    <img src={item.signedUrl} style={{ width: '600px' }} className='center' />
                     <button className="likeButton" onClick={() => handleLike(item.likes, item.id)} ><FaDumbbell /> &nbsp; {item.likes}</button>
                     <div className="caption">{item.caption}</div>
-                    <div>Comment:
-                    <input className="commentInput"
+                    <div className='commentSubmission'>Comment: &nbsp;
+                        <input className="commentInput"
                             type="text"
                             id="textInput"
                             name="comment"
                             value={comment}
                             onChange={handleCommentInput}
                         />
+                          <button className="postComment" onClick={() => handleCommentSubmit(item.id)}>Post Comment</button>
                     </div>
-                    <button onClick={() => handleCommentSubmit(item.id)}>Post Comment</button>
+                  
                 </div>
             ))}
         </div>
     );
 }
-
-//'${item.id}', '${item.likes}'
+//need to implement comment display option 
