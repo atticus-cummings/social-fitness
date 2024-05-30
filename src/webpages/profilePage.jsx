064945e41695b4cc6793e9a5a64bd1df0630d2dc
Manav Ganger <11431811+Manavganger@users.noreply.github.com>
2024-05-29 19:51:23 -0700

    Pass Session to Profile Page

--- src/webpages/profilePage.jsx
+++ src/webpages/profilePage.jsx
@@ -2,13 +2,13 @@ import React from "react"
 import {Taskbar} from "../components/taskbar"
 import  Profile from "../Profile"
 
-export const ProfilePage = () => {
+export const ProfilePage = ( {session, supabase} ) => {
     return(
         <>
         <div>
             <h1>PROFILE PAGE</h1>
         </div>
-        <Profile></Profile>
+        <Profile session={session} supabase={supabase}/>
         <Taskbar></Taskbar>
         </>
 
