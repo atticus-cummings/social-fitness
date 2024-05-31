b5c7a9b48eeee10fcb3e4b9269e7e564c2b03b57
Atticus Cummings <atticuscummings@ucla.edu>
2024-05-30 17:30:36 -0700

    Create workout stats post template page. Display template only, upload and feed display not implemented

--- src/uploadTemplates/workoutPost.js
+++ src/uploadTemplates/workoutPost.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function WorkoutPost({ supabase, session }) {
-    return(
+    const userId = session.user.id;
+    const [userData, setUserData] = useState(null);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const { data: userImport, error: userStatsError } = await supabase
+                    .from('fitness_stats')
+                    .select()
+                    .eq('user_id', userId)
+
+                console.log('existing stats:', userImport);
+
+                if (!userImport || userImport.length === 0) {
+                    console.log("null?");
+                    await supabase
+                        .from('fitness_stats')
+                        .insert([{ user_id: userId }])
+                        .throwOnError();
+                    console.log('New record inserted for user:', userId);
+                } else {
+                    console.log('User stats already exist for user:', userId);
+                    setUserData(userImport[0]);
+                }
+            } catch (error) {
+                console.error('Error fetching or inserting data:', error.message);
+                throw error; // Throw the error for the calling function to handle
+            }
+        }
+        fetchData();
+    }, [userId]);
+
+    useEffect(() => {
+        if (userData) {
+            if (userData.num_entries === null) {
+                setIndex(-1);
+            } else {
+                setIndex(userData.num_entries - 1);
+            }
+        }
+    }, [userData]);
+
+    return (
         <div>
-            <div>Workout </div>
-            <div>This info will be inputted into profile. You'll have an option to automattically post a formatted version here</div>
+            <div>
+                <div> Include your  Latest Workout stats: </div>
+                {userData !== null ? (
+                    <div>
+                        {index !== -1 && (
+                            <div>
+                                <div>
+                                    <input type="checkbox" id="bench" name="bench" />
+                                    <label for="bench"> Bench Press {userData.bench[index]} lbs</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" id="deadlift" name="deadlift" />
+                                    <label for="deadlift"> Deadlift {userData.deadlift[index]} lbs</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" id="mile_time" name="mile_time" />
+                                    <label for="mile_time"> Mile Time {userData.mile_time[index]}</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" id="pullups" name="pullups" />
+                                    <label for="pullups"> Pullups {userData.pullups[index]}</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" id="pushups" name="pushups" />
+                                    <label for="pushups"> Pushups {userData.pushups[index]}</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" id="weight" name="weight" />
+                                    <label for="weight"> Weight {userData.weight[index]} lbs</label>
+                                </div>
+
+                                <div>
+                                    <input type="checkbox" id="height" name="height" />
+                                    <label for="height"> Height {userData.height[index]} inches</label>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                ) : null}
+            </div>
         </div>
-        
-    )
+    );
 }
--- src/webpages/fitnessStatsInput.js
+++ src/webpages/fitnessStatsInput.js
@@ -12,56 +12,56 @@ export default function FitnessStats({ supabase, session }) {
     const [pullups, setPullups] = useState(0);
     const [weight, setWeight] = useState(0);
     const [height, setHeight] = useState(0);
-    const[lastIndex, setLastIndex] = useState(0);
+    const [lastIndex, setLastIndex] = useState(0);
     useEffect(() => {
-    async function fetchData() {
-        try {
-          // Fetch user stats
-          const { data: userStats, error: userStatsError } = await supabase
-            .from('fitness_stats')
-            .select('user_id, num_entries, deadlift, bench, mile_time, pullups, pushups, weight, height, update_timestamp')
-            .eq('user_id', userId)
+        async function fetchData() {
+            try {
+                // Fetch user stats
+                const { data: userStats, error: userStatsError } = await supabase
+                    .from('fitness_stats')
+                    .select('user_id, num_entries, deadlift, bench, mile_time, pullups, pushups, weight, height, update_timestamp')
+                    .eq('user_id', userId)
 
-        console.log('existing stats:', userStats);
+                console.log('existing stats:', userStats);
 
-          if (!userStats || userStats.length === 0) {
-            console.log("null?");
-            await supabase
-              .from('fitness_stats')
-              .insert([{ user_id: userId }])
-              .throwOnError(); 
-              console.log('New record inserted for user:', userId);
-            } else {
-              console.log('User stats already exist for user:', userId);
-            setUserData(userStats[0]); //for some reason it has t obe in this stupid form I hate this 
-            console.log("userDAta", userData)
-            if(userData.num_entries === null){
-                  setLastIndex(-1);
-            } else {
+                if (!userStats || userStats.length === 0) {
+                    console.log("null?");
+                    await supabase
+                        .from('fitness_stats')
+                        .insert([{ user_id: userId }])
+                        .throwOnError();
+                    console.log('New record inserted for user:', userId);
+                } else {
+                    console.log('User stats already exist for user:', userId);
+                    setUserData(userStats[0]); //for some reason it has t obe in this stupid form I hate this 
+                    console.log("userDAta", userData)
+                    if (userData.num_entries === null) {
+                        setLastIndex(-1);
+                    } else {
 
-                setLastIndex(userData.num_entries -1);
-                console.log("this stupid fucking index",userData.num_entries);
+                        setLastIndex(userData.num_entries - 1);
+                        console.log("this stupid fucking index", userData.num_entries);
+                    }
+
+                    if (lastIndex >= 0) {
+                        if (userData.deadlift && userData.deadlift[lastIndex] !== null) setDeadlift(userData.deadlift[lastIndex]);
+                        if (userData.bench && userData.bench[lastIndex] !== null) setBench(userData.bench[lastIndex]);
+                        if (userData.mile_time && userData.mile_time[lastIndex] !== null) setMileTime(userData.mile_time[lastIndex]);
+                        if (userData.pullups && userData.pullups[lastIndex] !== null) setPullups(userData.pullups[lastIndex]);
+                        if (userData.pushups && userData.pushups[lastIndex] !== null) setPushups(userData.pushups[lastIndex]);
+                        if (userData.weight && userData.weight[lastIndex] !== null) setWeight(userData.weight[lastIndex]);
+                        if (userData.height && userData.height[lastIndex] !== null) setHeight(userData.height[lastIndex]);
+                    } else {
+                        console.log('User stats arrays are empty or not properly defined.');
+                    }
+                }
+                //setUserData(userData); // Return fetched user stats
+            } catch (error) {
+                console.error('Error fetching or inserting data:', error.message);
+                throw error; // Throw the error for the calling function to handle
             }
-        
-              if (lastIndex >= 0) {
-                if (userData.deadlift && userData.deadlift[lastIndex] !== null) setDeadlift(userData.deadlift[lastIndex]);
-                if (userData.bench && userData.bench[lastIndex] !== null) setBench(userData.bench[lastIndex]);
-                if (userData.mile_time && userData.mile_time[lastIndex] !== null) setMileTime(userData.mile_time[lastIndex]);
-                if (userData.pullups && userData.pullups[lastIndex] !== null) setPullups(userData.pullups[lastIndex]);
-                if (userData.pushups && userData.pushups[lastIndex] !== null) setPushups(userData.pushups[lastIndex]);
-                if (userData.weight && userData.weight[lastIndex] !== null) setWeight(userData.weight[lastIndex]);
-                if (userData.height && userData.height[lastIndex] !== null) setHeight(userData.height[lastIndex]);
-              } else {
-                console.log('User stats arrays are empty or not properly defined.');
-              }
-            }
-            //setUserData(userData); // Return fetched user stats
-        } catch (error) {
-          console.error('Error fetching or inserting data:', error.message);
-          throw error; // Throw the error for the calling function to handle
         }
-      }
-      fetchData();
+        fetchData();
     }, [userId]);
 
 
@@ -72,24 +72,24 @@ export default function FitnessStats({ supabase, session }) {
         let pushupsArray;
         let pullupsArray;
         let weightArray;
-        let heightArray; 
+        let heightArray;
         let timestampArray;
         let timestamp = new Date();
 
- //I never thought formatting a date would give me so much trouble--this is infuriating 
- //I've tried everything to get it to input into the supabase date format. I just ended up making the timestamp log a string input. 
+        //I never thought formatting a date would give me so much trouble--this is infuriating 
+        //I've tried everything to get it to input into the supabase date format. I just ended up making the timestamp log a string input. 
         console.log("index", lastIndex);
         console.log("userData", userData);
-        if(lastIndex < 0){
+        if (lastIndex < 0) {
             deadliftArray = [deadlift];
-            benchArray = [ bench];
+            benchArray = [bench];
             mileTimeArray = [mileTime];
             pushupsArray = [pushups];
             pullupsArray = [pullups];
             weightArray = [weight];
-            heightArray = [height]; 
+            heightArray = [height];
             timestampArray = [timestamp.toISOString()];
-        } else{
+        } else {
             benchArray = [...userData.bench, bench];
             deadliftArray = [...userData.deadlift, deadlift];
             mileTimeArray = [...userData.mile_time, mileTime];
@@ -103,46 +103,46 @@ export default function FitnessStats({ supabase, session }) {
         console.log("deadlift", deadliftArray);
         await supabase
             .from('fitness_stats')
-            .update({ num_entries:(userData.num_entries +1), update_timestamp:timestampArray, deadlift:deadliftArray, bench:benchArray, pushups:pushupsArray, mile_time:mileTimeArray, pullups:pullupsArray, weight:weightArray, height:heightArray})
+            .update({ num_entries: (userData.num_entries + 1), update_timestamp: timestampArray, deadlift: deadliftArray, bench: benchArray, pushups: pushupsArray, mile_time: mileTimeArray, pullups: pullupsArray, weight: weightArray, height: heightArray })
             .eq('user_id', userId)
             .throwOnError();
         console.log("submitted?");
 
         return;
     };
-    return(
+    return (
         <div className="statsPage">
-        <div className="fitnessCategory">
-            Deadlift
-            <input className="fitnessInput" type="number" id="deadlift" name="deadlift" value={deadlift} onChange={(event) => setDeadlift(event.target.value)} />
-        </div >
-        <div  className="fitnessCategory">
-            Bench
-            <input className="fitnessInput" type="number" id="bench" name="bench" value={bench} onChange={(event) => setBench(event.target.value)} />
-        </div>
-        <div  className="fitnessCategory">
-            miletime
-            <input className="fitnessInput" type="text" id="mileTime" name="mileTime" value={mileTime} onChange={(event) => setMileTime(event.target.value)} />
-        </div>
-        <div  className="fitnessCategory">
-            Pushups
-            <input className="fitnessInput" type="number" id="pushups" name="pushups" value={pushups} onChange={(event) => setPushups(event.target.value)} />
-        </div>
-        <div  className="fitnessCategory">
-            Pullups
-            <input className="fitnessInput" type="number" id="pullups" name="pullups" value={pullups} onChange={(event) => setPullups(event.target.value)} />
-        </div>
-        <div  className="fitnessCategory">
-            Weight
-            <input className="fitnessInput" type="number" id="weight" name="weight" value={weight} onChange={(event) => setWeight(event.target.value)} />
-        </div>
-        <div  className="fitnessCategory">
-            Height
-            <input className="fitnessInput" type="text" id="height" name="height" value={height} onChange={(event) => setHeight(event.target.value)} />
-        </div>
-        <div className="submitButton">
-            <button onClick={() => handleSubmit(userData)}>Post</button>
-        </div>
+            <div className="fitnessCategory">
+                Deadlift
+                <input className="fitnessInput" type="number" id="deadlift" name="deadlift" value={deadlift} onChange={(event) => setDeadlift(event.target.value)} />
+            </div >
+            <div className="fitnessCategory">
+                Bench
+                <input className="fitnessInput" type="number" id="bench" name="bench" value={bench} onChange={(event) => setBench(event.target.value)} />
+            </div>
+            <div className="fitnessCategory">
+                miletime
+                <input className="fitnessInput" type="text" id="mileTime" name="mileTime" value={mileTime} onChange={(event) => setMileTime(event.target.value)} />
+            </div>
+            <div className="fitnessCategory">
+                Pushups
+                <input className="fitnessInput" type="number" id="pushups" name="pushups" value={pushups} onChange={(event) => setPushups(event.target.value)} />
+            </div>
+            <div className="fitnessCategory">
+                Pullups
+                <input className="fitnessInput" type="number" id="pullups" name="pullups" value={pullups} onChange={(event) => setPullups(event.target.value)} />
+            </div>
+            <div className="fitnessCategory">
+                Weight
+                <input className="fitnessInput" type="number" id="weight" name="weight" value={weight} onChange={(event) => setWeight(event.target.value)} />
+            </div>
+            <div className="fitnessCategory">
+                Height
+                <input className="fitnessInput" type="text" id="height" name="height" value={height} onChange={(event) => setHeight(event.target.value)} />
+            </div>
+            <div className="submitButton">
+                <button onClick={() => handleSubmit(userData)}>Post</button>
+            </div>
         </div>
     )
 }
