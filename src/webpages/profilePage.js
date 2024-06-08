1e0c542034ee09be45277a3a2d47c7187f2ff32d
Atticus Cummings <atticuscummings@ucla.edu>
2024-06-07 17:05:37 -0700

    Fix public/private profile data loading

--- src/webpages/profilePage.js
+++ src/webpages/profilePage.js
@@ -1,4 +1,4 @@
-import {Taskbar} from "../components/taskbar"
+import { Taskbar } from "../components/taskbar"
 import FitnessStatsInput from "./fitnessStatsInput";
 import "./profile.css";
 import React, { useState, useEffect } from 'react';
@@ -9,7 +9,7 @@ import DefaultPostDisplay from "../components/postDisplays/defaultPostDisplay";
 import TextPostDisplay from "../components/postDisplays/textPostDisplay";
 import StatPostDisplay from "../components/postDisplays/statPostDisplay";
 
-export const ProfilePage = ( {session, supabase} ) => {
+export const ProfilePage = ({ session, supabase }) => {
   const [email, setEmail] = useState('');
   const [currentEmail, setCurrentEmail] = useState('');
   const [profileUrl, setProfileUrl] = useState('');
@@ -18,9 +18,9 @@ export const ProfilePage = ( {session, supabase} ) => {
   const [username, setUsername] = useState('');
   const [currentUsername, setCurrentUsername] = useState('');
   const [loading, setLoading] = useState(false);
-  const [isPublic, setIsPublic] = useState(false);
-  const [showPublic, setShowPublic] = useState(false);
-  
+  const [isPublic, setIsPublic] = useState(null);
+  const [showPublic, setShowPublic] = useState(null);
+
   const [showProfile, setShowProfile] = useState(false);
   const [profileData, setProfileData] = useState(null);
 
@@ -31,8 +31,7 @@ export const ProfilePage = ( {session, supabase} ) => {
     if (session && session.user) {
       setCurrentEmail(session.email);
       setProfileUrl(session.user.user_metadata.avatar_url || '');
-      fetchPublicStatus();
-      fetchShowPublicStatus();
+ 
       // Fetch and set the current username
 
     }
@@ -50,7 +49,7 @@ export const ProfilePage = ( {session, supabase} ) => {
     setUsername(event.target.value);
   };
 
-  
+
   const fetchPublicStatus = async () => {
     try {
       const { data, error } = await supabase
@@ -94,16 +93,16 @@ export const ProfilePage = ( {session, supabase} ) => {
       // setMessage('No user is logged in.');
     }
   };
-  
+
   const setShowPublicChange = async () => {
     const newShowPublic = !showPublic;
     console.log("public post display change");
-      if (userId) {
-        const { error } = await supabase
-          .from('profiles')
-          .update({see_public: newShowPublic })
-          .eq('id', userId);
-      
+    if (userId) {
+      const { error } = await supabase
+        .from('profiles')
+        .update({ see_public: newShowPublic })
+        .eq('id', userId);
+
       if (error) {
         setMessage('Set show public ' + error.message);
       } else {
@@ -124,7 +123,7 @@ export const ProfilePage = ( {session, supabase} ) => {
       // setMessage('No user is logged in.');
     }
   };
-  
+
   const setPublicStatusChange = async () => {
     console.log("public status change");
     if (isPublic) {
@@ -161,7 +160,7 @@ export const ProfilePage = ( {session, supabase} ) => {
     if (error) {
       // setMessage('Failed to update email: ' + error.message);
     } else {
-     // setMessage('Email updated successfully. Please check your inbox to verify your new email.');
+      // setMessage('Email updated successfully. Please check your inbox to verify your new email.');
       setCurrentEmail(email);
     }
   };
@@ -175,7 +174,7 @@ export const ProfilePage = ( {session, supabase} ) => {
       .update({ username: username })
       .match({ id: userId });
     if (error) {
-      
+
     } else {
       // setMessage('Username updated successfully!');
       setCurrentUsername(username);
@@ -218,7 +217,7 @@ export const ProfilePage = ( {session, supabase} ) => {
       setCurrentUsername('No username set');
     }
   }
-  
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -257,7 +256,7 @@ export const ProfilePage = ( {session, supabase} ) => {
     const { publicURL, error: urlError } = await supabase.storage.from('media').getPublicUrl(filePath);
 
     if (urlError) {
-     // setMessage('Failed to get image URL: ' + urlError.message);
+      // setMessage('Failed to get image URL: ' + urlError.message);
       return;
     }
 
@@ -265,151 +264,155 @@ export const ProfilePage = ( {session, supabase} ) => {
     setMessage('Profile image updated successfully.');
 
 
-    const {data, error: fileNameError} = await supabase
+    const { data, error: fileNameError } = await supabase
       .from('avatars')
-      .upsert({user_id: userId, file_name: fileName})
+      .upsert({ user_id: userId, file_name: fileName })
       .throwOnError()
   };
   async function fetchData() {
     await getEmail();
     await getUserName();
     await fetchFitnessStats();
-
+    await fetchPublicStatus();
+    await fetchShowPublicStatus();
 
 
     const { data: file_name, error: fileError } = await supabase
       .from('avatars')
-      .select('file_name') 
+      .select('file_name')
       .eq('user_id', userId)
       .throwOnError()
-    if (file_name){
+    if (file_name) {
       const { data, error } = await supabase
         .storage
         .from('media')
-        .createSignedUrl(`/avatars/${file_name[0].file_name}`, 60) 
-        setProfileUrl(data.signedUrl)
+        .createSignedUrl(`/avatars/${file_name[0].file_name}`, 60)
+      setProfileUrl(data.signedUrl)
     }
 
   }
 
   async function fetchFitnessStats() {
     try {
-        const { data: followingUserIdsData, error: followingError } = await supabase //this puts it in the format necessary for the FetchPostData function.
+      const { data: followingUserIdsData, error: followingError } = await supabase //this puts it in the format necessary for the FetchPostData function.
         //--is a bit of a pain. Might want to change later 
-            .from('followers')
-            .select('user_id')
-            .eq('user_id', userId);
-        if (followingError) throw followingError;
+        .from('followers')
+        .select('user_id')
+        .eq('user_id', userId);
+      if (followingError) throw followingError;
 
-        const followingUserIds = followingUserIdsData.map(user => user.user_id);
-        followingUserIds.push(userId);
+      const followingUserIds = followingUserIdsData.map(user => user.user_id);
+      followingUserIds.push(userId);
 
-        /*              From feed.js                */
-        // Fetch all the UUIDs of users the current user is following
-        /* #####################    Fetch User's Following List  ##################### */
+      /*              From feed.js                */
+      // Fetch all the UUIDs of users the current user is following
+      /* #####################    Fetch User's Following List  ##################### */
 
-        // Fetch file metadata
-        /* #####################    Fetch User's Liked Post list  ##################### */
-            const combinedData = await FetchPostData(session,supabase,followingUserIds)
-            setShowProfile(true);
-            setProfileData(combinedData);
+      // Fetch file metadata
+      /* #####################    Fetch User's Liked Post list  ##################### */
+      const combinedData = await FetchPostData(session, supabase, followingUserIds)
+      setShowProfile(true);
+      setProfileData(combinedData);
     } catch (error) {
-        console.error('Error fetching data:', error);
-        return null;
+      console.error('Error fetching data:', error);
+      return null;
     }
-}
+  }
 
-return (
+  return (
 
-  <div className="top-cont">
-    <h1>PROFILE</h1>
-  <div className="profile-container">
-  
-      
- 
-  <div className="profile-content">
-      <div className="profile-info-section">
-          <div className="profile-update-email">
+    <div className="top-cont">
+      <h1>PROFILE</h1>
+      <div className="profile-container">
+
+
+
+        <div className="profile-content">
+          <div className="profile-info-section">
+            <div className="profile-update-email">
               <p className="profile-info">Current Email: {currentEmail}</p>
               <div className="input-group">
-                  <input
-                      type="email"
-                      value={email}
-                      className="input-field"
-                      onChange={handleEmailChange}
-                      placeholder="Enter new email"
-                  />
-                  <button className="btn-update" onClick={updateEmail}>Update</button>
+                <input
+                  type="email"
+                  value={email}
+                  className="input-field"
+                  onChange={handleEmailChange}
+                  placeholder="Enter new email"
+                />
+                <button className="btn-update" onClick={updateEmail}>Update</button>
               </div>
+            </div>
+
           </div>
+          <div className="profile-info-section">
+            {isPublic !== null &&
+              <div className="profile-public-status">
+                <p>Account Status: {isPublic ? "Public" : "Private"}</p>
+                <label className="switch">
+                  <button className="public-button" onClick={handlePublicStatusChange}
+                  >Click to set {isPublic ? "private" : "public"}</button>
+                  <span className="slider round"></span>
+                </label>
+              </div>
+            }
+            {showPublic !== null &&
+              <div className="profile-public-status">
+                <p>Currently {!showPublic ? "not displaying public accounts" : "displaying public accounts"}</p>
+                <label className="switch">
+                  <button className="public-button" onClick={handleShowPublicChange}
+                  >Click to set: {!showPublic ? "show public accounts" : "don't show public accounts"}</button>
+                  <span className="slider round"></span>
+                </label>
+              </div>
+            }
+          </div>
+          <div className="profile-info-section">
 
-</div>
-<div className="profile-info-section">
-        <div className="profile-public-status">
-          <p>Account Status: {isPublic ? "Public" : "Private"}</p>
-          <label className="switch">
-            <button  className="public-button"    onClick={handlePublicStatusChange}
-            >Click to set {isPublic ? "private" : "public" }</button>
-            <span className="slider round"></span>
-          </label>
-        </div>
-        <div className="profile-public-status">
-          <p>Currently {!showPublic ? "not displaying public accounts" : "displaying public accounts"}</p>
-          <label className="switch">
-            <button  className="public-button"    onClick={handleShowPublicChange}
-            >Click to set: {!showPublic ? "show public accounts": "don't show public accounts"  }</button>
-            <span className="slider round"></span>
-          </label>
-        </div>
-      </div>
+            <div className="profile-update-username">
 
-      <div className="profile-info-section">
-
-      <div className="profile-update-username">
-           
               <p>Current Username: {currentUsername}</p>
               <form className="form-update" onSubmit={handleSubmit}>
-                  <input
-                      type="text"
-                      id="username"
-                      value={username}
-                      className="input-field"
-                      onChange={handleUsernameChange}
-                      placeholder="Enter new username"
-                  />
-                  <button type="submit" className="btn-update" >
-                      Update
-                  </button>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  className="input-field"
+                  onChange={handleUsernameChange}
+                  placeholder="Enter new username"
+                />
+                <button type="submit" className="btn-update" >
+                  Update
+                </button>
               </form>
+            </div>
           </div>
-      </div>
-      <div className="profile-update-image">
-          {profileUrl && (
+          <div className="profile-update-image">
+            {profileUrl && (
               <div className="image-preview">
-                  <img src={profileUrl} alt="Profile" />
+                <img src={profileUrl} alt="Profile" />
               </div>
-          )}
-          <input type="file" onChange={handleFileChange} className="input-file"/>
-          {message && <p>{message}</p>}
-      </div>
-      </div>
-      
-  </div>
-          
+            )}
+            <input type="file" onChange={handleFileChange} className="input-file" />
+            {message && <p>{message}</p>}
+          </div>
+        </div>
 
-      <center><FitnessStatsInput session={session} supabase={supabase}/></center>
+      </div>
+
+
+      <center><FitnessStatsInput session={session} supabase={supabase} /></center>
       <div className="profilePostPage">
-      {profileData === null ? <>You have no data to show!</> : profileData?.map((item, index) => (
-                <div className="profilePost">
-                    {item.post_type === 1 && <DefaultPostDisplay session={session} supabase={supabase} item={item} index={index} size={'600px'} />}
-                    {item.post_type === 2 && <TextPostDisplay session={session} supabase={supabase} item={item} index={index} size={'600px'} />}
-                    {item.post_type === 3 && <StatPostDisplay session={session} supabase={supabase} item={item} index={index} size={'600px'} />}
-                </div>
-            ))}
-      <Taskbar></Taskbar>
+        {profileData === null ? <>You have no data to show!</> : profileData?.map((item, index) => (
+          <div className="profilePost">
+            {item.post_type === 1 && <DefaultPostDisplay session={session} supabase={supabase} item={item} index={index} size={'600px'} />}
+            {item.post_type === 2 && <TextPostDisplay session={session} supabase={supabase} item={item} index={index} size={'600px'} />}
+            {item.post_type === 3 && <StatPostDisplay session={session} supabase={supabase} item={item} index={index} size={'600px'} />}
+          </div>
+        ))}
+        <Taskbar></Taskbar>
       </div>
     </div>
 
- 
+
   );
 }
